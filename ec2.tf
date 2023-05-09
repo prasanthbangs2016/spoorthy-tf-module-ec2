885fba735e124186381ee5c21abfb4218ef7dc30
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-05-09 23:27:31 +0530

    added

---
+++ ec2.tf
@@ -0,0 +1,17 @@
+resource "aws_spot_instance_request" "spoorthy_vm" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  iam_instance_profile = aws_iam_instance_profile.s3_bucket
+  wait_for_fulfillment = true
+
+  tags = {
+    Name = "${var.spoothy_bucket_tag_name}-vm"
+  }
+}
+
+
+resource "aws_ec2_tag" "name" {
+  resource_id = aws_spot_instance_request.spoorthy_vm.spot_instance_id
+  key = "Name"
+  value = "${var.spoothy_bucket_tag_name}"
+}
---
+++ iam.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_role" "s3_bucket" {
+  name = "spoorthy-${var.spoothy_bucket_tag_env}_s3_full_access"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+      tag-key = "spoorthy-${var.spoothy_bucket_tag_env}_s3_full_access"
+  }
+}
