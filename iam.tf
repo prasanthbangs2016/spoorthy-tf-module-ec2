2a91b59e798230bb3f6b43d8645de158f2b78bc0
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-05-10 02:41:12 +0530

    added

--- iam.tf
+++ iam.tf
@@ -3,13 +3,20 @@ resource "aws_iam_policy" "s3-access" {
   path        = "/"
   description = "spoorthy-${var.spoothy-bucket-tag-env}-s3-full-access"
 
-  policy = jsonencode({
-      sid = "S3FullAccess"
-
-      effect    = "Allow"
-      actions   = ["s3:*"]
-      resources = ["*"]
-    })
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_iam_role" "s3-bucket" {
