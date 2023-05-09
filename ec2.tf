0df1b20780cef3b5242e78e0b22bbd7def5fb58c
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-05-10 00:08:08 +0530

    added

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "spoorthy_vm" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "${var.instance_type}"
-  iam_instance_profile = aws_iam_instance_profile.s3_bucket
+  iam_instance_profile = aws_iam_instance_profile.s3_bucket.name
   wait_for_fulfillment = true
 
   tags = {
